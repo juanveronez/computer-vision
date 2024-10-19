f94cde8a82de4b1e610a55af1ffef2659dcd6b39
Juan Veronez <jmonteiroveronez@apple.com>
2024-10-19 11:43:53 -0300

    updating transform script

--- src/scripts/transform_data.py
+++ src/scripts/transform_data.py
@@ -1,5 +1,5 @@
 from image_transformer import CategorizedImageTransformerBuilder
-from sklearn.model_selection import StratifiedShuffleSplit
+
 import os
 from pathlib import Path
 import numpy as np
@@ -18,28 +18,8 @@ X, y = CategorizedImageTransformerBuilder() \
   .add_label('U', 'U_u/train_55') \
   .build(lambda y: (y == 'i') | (y == 'I'))
 
-def train_test_split(X, y):
-  split_test_threshold = 0.2
-  selection_iter = StratifiedShuffleSplit(n_splits=1, test_size=split_test_threshold, random_state=42)
-
-  train_index, test_index = next(selection_iter.split(X, y))
-
-  X_train = X[train_index]
-  X_test = X[test_index]
-  y_train = y[train_index]
-  y_test = y[test_index]
-
-  return X_train, X_test, y_train, y_test
-
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
 def save_file(file_name, data):
   np.save(f'../data_processed/{file_name}.npy', data)
 
-
 save_file('X', X)
 save_file('y', y)
-save_file('X_train', X_train)
-save_file('X_test', X_test)
-save_file('y_train', y_train)
-save_file('y_test', y_test)
